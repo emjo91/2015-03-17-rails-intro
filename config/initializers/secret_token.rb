139f828d07608a235f87624138f145c30dce6663
EmilyJohnson <3mle33@gmail.com>
2015-03-17 12:03:58 -0500

    Got the basics of rails set up! And even created some products!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFails::Application.config.secret_token = '395b171e463e7eccff8c38fa563e700800b5a2de5cfea795127985c2af4d26aa723da5f61c7eae32ba3653e08882e4dd88a2d8322d411a664905b7d52834e596'
